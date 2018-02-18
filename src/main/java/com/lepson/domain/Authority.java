5e3de50a7defe40fc1a55b588c3f7a4af37ec034
Michał Łepecki <>
2018-02-18 18:11:24 +0100

    Dodanie kolejnego pola w klasie. Utworzenie nowej klasy Usernn.

--- src/main/java/com/lepson/domain/Authority.java
+++ src/main/java/com/lepson/domain/Authority.java
@@ -27,12 +27,22 @@ public class Authority implements Serializable {
     @Column(length = 50)
     private String name;
 
+    private String age;
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
     }
 
     @Override
